9007587f73c1016a5753ead098094dacde9f341f
ErickCruz23 <holasoyunnombre23@gmail.com>
2018-05-18 02:22:21 -0500

    Nada importante

--- app/src/main/java/edu/cecyt9/ipn/movil_link2band/SecurityMechanism.java
+++ app/src/main/java/edu/cecyt9/ipn/movil_link2band/SecurityMechanism.java
@@ -469,7 +469,7 @@ public class SecurityMechanism extends Fragment implements View.OnClickListener,
                         }
                     }).show();
             dialog.dismiss();
-            actualizaLoc(latitud + ", " + longitud, "Desactivado", SecMode);
+            actualizaLoc(latitud + ", " + longitud, "Desbloqueado", SecMode);
             DatabaseHelper DB = new DatabaseHelper(getActivity());
             DB.actualiza(Comands.getID(), latitud + ", " + longitud);
         }
